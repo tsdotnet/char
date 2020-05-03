533615e9a766aaeb3a6f85da92248979342f1fd7
Oren (electricessence) <oren@electrified.net>
2020-05-03 12:22:50 -0700

    Initial release.

---
+++ dist-esm/index.js
@@ -0,0 +1,42 @@
+/*!
+ * @author electricessence / https://github.com/electricessence/
+ * From Netjs mscorlib.ts
+ * Licensing: MIT
+ */
+/**
+ * Returns true if the character code is white-space.
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export function isWhiteSpace(ch) {
+    return ch === 32 || (ch >= 9 && ch <= 13) || ch === 133 || ch === 160;
+}
+/**
+ * Returns true if the character code is an alpha letter (case insensitive).
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export function isLetter(ch) {
+    return (65 <= ch && ch <= 90) || (97 <= ch && ch <= 122) || (ch >= 128 && ch !== 133 && ch !== 160);
+}
+/**
+ * Returns true if the character code is alphanumeric.
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export function isLetterOrDigit(ch) {
+    return ((48 <= ch && ch <= 57) ||
+        (65 <= ch && ch <= 90) ||
+        (97 <= ch && ch <= 122) ||
+        (ch >= 128 && ch !== 133 && ch !== 160));
+}
+export function isDigit(chOrStr, index) {
+    if (index == null) {
+        return 48 <= chOrStr && chOrStr <= 57;
+    }
+    else {
+        const ch = chOrStr.charCodeAt(index);
+        return 48 <= ch && ch <= 57;
+    }
+}
+//# sourceMappingURL=index.js.map
---
+++ dist/index.d.ts
@@ -0,0 +1,30 @@
+/*!
+ * @author electricessence / https://github.com/electricessence/
+ * From Netjs mscorlib.ts
+ * Licensing: MIT
+ */
+/**
+ * Returns true if the character code is white-space.
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export declare function isWhiteSpace(ch: number): boolean;
+/**
+ * Returns true if the character code is an alpha letter (case insensitive).
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export declare function isLetter(ch: number): boolean;
+/**
+ * Returns true if the character code is alphanumeric.
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export declare function isLetterOrDigit(ch: number): boolean;
+/**
+ * Returns true if the character code is a number (0-9).
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export declare function isDigit(ch: number): boolean;
+export declare function isDigit(str: string, index: number): boolean;
