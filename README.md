146e4af029f90e71f93bc4c3fa9625e04d3c0cfd
Oren (electricessence) <oren@electrified.net>
2020-05-03 12:22:40 -0700

    Initial release.

---
+++ README.md
@@ -0,0 +1,7 @@
+# ![alt text](https://avatars1.githubusercontent.com/u/64487547?s=30&amp;v=4 "tsdotnet") tsdotnet / char
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)](https://github.com/tsdotnet/char/blob/master/LICENSE)
+![npm-publish](https://github.com/tsdotnet/char/workflows/npm-publish/badge.svg)
+[![npm version](https://img.shields.io/npm/v/@tsdotnet/char.svg?style=flat-square)](https://www.npmjs.com/package/@tsdotnet/char)
+
+A set of functions for testing character codes.
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+/*!
+ * @author electricessence / https://github.com/electricessence/
+ * From Netjs mscorlib.ts
+ * Licensing: MIT
+ */
+
+/**
+ * Returns true if the character code is white-space.
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export function isWhiteSpace (ch: number): boolean
+{
+	return ch===32 || (ch>=9 && ch<=13) || ch===133 || ch===160;
+}
+
+/**
+ * Returns true if the character code is an alpha letter (case insensitive).
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export function isLetter (ch: number): boolean
+{
+	return (65<=ch && ch<=90) || (97<=ch && ch<=122) || (ch>=128 && ch!==133 && ch!==160);
+}
+
+/**
+ * Returns true if the character code is alphanumeric.
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export function isLetterOrDigit (ch: number): boolean
+{
+	return (
+		(48<=ch && ch<=57) ||
+		(65<=ch && ch<=90) ||
+		(97<=ch && ch<=122) ||
+		(ch>=128 && ch!==133 && ch!==160)
+	);
+}
+
+/**
+ * Returns true if the character code is a number (0-9).
+ * @param {number} ch
+ * @returns {boolean}
+ */
+export function isDigit (ch: number): boolean;
+export function isDigit (str: string, index: number): boolean;
+export function isDigit (chOrStr: any, index?: number): boolean
+{
+	if(index==null)
+	{
+		return 48<=chOrStr && chOrStr<=57;
+	}
+	else
+	{
+		const ch = chOrStr.charCodeAt(index);
+		return 48<=ch && ch<=57;
+	}
+}
