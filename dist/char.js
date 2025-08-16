3b79941de8592df09fdc69aa7ebe6e55c9c2d06e
electricessence <5899455+electricessence@users.noreply.github.com>
2025-08-15 17:35:49 -0700

    refactor: update TypeScript configuration and add character utility functions
    - Updated `tsconfig.esm.json` to extend from `tsconfig.json` and set module to `esnext`.
    - Modified `tsconfig.json` to change target to `es2022`, added `allowSyntheticDefaultImports`, and excluded certain directories from compilation.
    - Added new character utility functions in `dist/char.mjs` for checking whitespace, letters, digits, and alphanumeric characters.
    - Included source map for `char.mjs`.
    - Created a new ESLint configuration file to integrate TypeScript linting with recommended rules and ignore specific directories.

--- dist/char.js
+++ dist/char.js
@@ -5,7 +5,10 @@
  * Licensing: MIT
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isDigit = exports.isLetterOrDigit = exports.isLetter = exports.isWhiteSpace = void 0;
+exports.isWhiteSpace = isWhiteSpace;
+exports.isLetter = isLetter;
+exports.isLetterOrDigit = isLetterOrDigit;
+exports.isDigit = isDigit;
 /**
  * Returns true if the character code is white-space.
  * @param {number} ch
@@ -14,7 +17,6 @@ exports.isDigit = exports.isLetterOrDigit = exports.isLetter = exports.isWhiteSp
 function isWhiteSpace(ch) {
     return ch === 32 || (ch >= 9 && ch <= 13) || ch === 133 || ch === 160;
 }
-exports.isWhiteSpace = isWhiteSpace;
 /**
  * Returns true if the character code is an alpha letter (case insensitive).
  * @param {number} ch
@@ -23,7 +25,6 @@ exports.isWhiteSpace = isWhiteSpace;
 function isLetter(ch) {
     return (65 <= ch && ch <= 90) || (97 <= ch && ch <= 122) || (ch >= 128 && ch !== 133 && ch !== 160);
 }
-exports.isLetter = isLetter;
 /**
  * Returns true if the character code is alphanumeric.
  * @param {number} ch
@@ -35,7 +36,6 @@ function isLetterOrDigit(ch) {
         (97 <= ch && ch <= 122) ||
         (ch >= 128 && ch !== 133 && ch !== 160));
 }
-exports.isLetterOrDigit = isLetterOrDigit;
 function isDigit(chOrStr, index = 0) {
     if (typeof chOrStr == 'number') {
         return 48 <= chOrStr && chOrStr <= 57;
@@ -45,5 +45,4 @@ function isDigit(chOrStr, index = 0) {
         return 48 <= ch && ch <= 57;
     }
 }
-exports.isDigit = isDigit;
 //# sourceMappingURL=char.js.map
